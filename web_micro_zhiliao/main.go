ad627084b0c1ddd5acf0591504948277ab6b1d27
olderyi <oldyier@163.com>
2023-04-06 20:50:16 +0800

    尝试调用另一个服务中的内容

--- web_micro_zhiliao/main.go
+++ web_micro_zhiliao/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-micro/plugins/v4/registry/consul"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/client"
 	"go-micro.dev/v4/registry"
+	"go-micro.dev/v4/selector"
 	"go-micro.dev/v4/web"
+	"web_micro_zhiliao/router"
 )
 
 var (
@@ -19,11 +23,26 @@ func main() {
 		registry.Addrs("127.0.0.1:8500"),
 	)
 
+	// Create selector
+	mySelector := selector.NewSelector(
+		selector.SetStrategy(selector.RoundRobin),
+		selector.Registry(consulReg),
+	)
+	// Create service
+	rpcSerivce := micro.NewService(
+		micro.Client(client.NewClient()),
+		micro.Selector(mySelector),
+		//micro.Registry(consulReg),
+	)
+	rpcSerivce.Init()
+
+	rpcSerivce.Run()
+
 	// router
 	ginRouter := gin.Default()
 	// router组
+	router.SetRouter(ginRouter)
 
-	// Create service
 	srv := web.NewService(
 		web.Name(service),
 		web.Version(version),
